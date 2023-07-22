127c6e2c83b199202f0bb33d4b82cd20ca7b4b44
Shane Horton <102448086+Shanehortonm@users.noreply.github.com>
2023-07-21 20:52:01 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,16 @@
-# add your code here
+for num in range(1,101) :
+    string = ""
+    
+    if num % 3 == 0 and num % 5 == 0:
+        string = string + "FizzBuzz"
+    
+    elif num % 3 == 0:
+        string = string + "Fizz"
+        
+    elif num % 5 == 0:
+        string = string + "Buzz"
+        
+    else : string = string + str(num)
+        
+    print(string)
 
